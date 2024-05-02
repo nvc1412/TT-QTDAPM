f793b8d8dce198192896f9081013b09b57fd6f92
cuong <nvc14122002@gmail.com>
2024-05-02 09:49:54 +0700

    Code ket noi csdl

---
+++ ConnectDB/Connect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHangGiaDung.ConnectDB
+{
+    public class Connect
+    {
+        public SqlConnection conn = null;
+        public SqlConnection ConnectDataBase()
+        {
+            try
+            {
+                string connect = @"Data Source=localhost;Initial Catalog=CuaHangGiaDungKimNgan;Integrated Security=True";
+                conn = new SqlConnection(connect);
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Loi ket noi CSDL: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
