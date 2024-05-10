4f78168f982e7640dbced1cbbd86583b6b78080e
cuong <nvc14122002@gmail.com>
2024-05-10 08:03:26 +0700

    Code chuc nang Quan Ly Khach Hang

---
+++ Model/KhachHang.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHangGiaDung.Model
+{
+    public class KhachHang
+    {
+        private string _MaKH;
+        private string _TenKH;
+        private string _DiaChi;
+        private string _Sdt;
+
+        public string MaKH { get => _MaKH; set => _MaKH = value; }
+        public string TenKH { get => _TenKH; set => _TenKH = value; }
+        public string DiaChi { get => _DiaChi; set => _DiaChi = value; }
+        public string Sdt { get => _Sdt; set => _Sdt = value; }
+
+        public KhachHang() { }
+    }
+}
